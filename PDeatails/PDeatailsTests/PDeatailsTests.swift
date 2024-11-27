2a44bac08bbed97c74aa96ed8dbf4b03667f3665
Ahmino <mohd.ahmed.e01@gmail.com>
2024-11-27 08:08:06 +0300

    first commit

---
+++ PDeatails/PDeatailsTests/PDeatailsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PDeatailsTests.swift
+//  PDeatailsTests
+//
+//  Created by Ahmed Mohammed on 26/11/2024.
+//
+
+import Testing
+@testable import PDeatails
+
+struct PDeatailsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
